96eb2c934ade4195a8c94fefc53f72f8266e680d
DarshnaSoni <darshna@websoham.com>
2023-10-19 17:33:18 +0530

    Installed dependencies

---
+++ .next/static/chunks/app/about/page.js
@@ -0,0 +1,39 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([["app/about/page"],{
+
+/***/ "(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=H%3A%5Cwebsoham%5Cpekkoreact-10%5Cpekkoreact-10%5CPekkoReactNexJs%5Cpekko_template%5Cassets%5Cimages%5Cabout.jpg&server=false!":
+/*!********************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=H%3A%5Cwebsoham%5Cpekkoreact-10%5Cpekkoreact-10%5CPekkoReactNexJs%5Cpekko_template%5Cassets%5Cimages%5Cabout.jpg&server=false! ***!
+  \********************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./assets/images/about.jpg */ \"(app-pages-browser)/./assets/images/about.jpg\"))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvYnVpbGQvd2VicGFjay9sb2FkZXJzL25leHQtZmxpZ2h0LWNsaWVudC1lbnRyeS1sb2FkZXIuanM/bW9kdWxlcz1IJTNBJTVDd2Vic29oYW0lNUNwZWtrb3JlYWN0LTEwJTVDcGVra29yZWFjdC0xMCU1Q1Bla2tvUmVhY3ROZXhKcyU1Q3Bla2tvX3RlbXBsYXRlJTVDYXNzZXRzJTVDaW1hZ2VzJTVDYWJvdXQuanBnJnNlcnZlcj1mYWxzZSEiLCJtYXBwaW5ncyI6IkFBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLz8xNmNlIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCgvKiB3ZWJwYWNrTW9kZTogXCJlYWdlclwiICovIFwiSDpcXFxcd2Vic29oYW1cXFxccGVra29yZWFjdC0xMFxcXFxwZWtrb3JlYWN0LTEwXFxcXFBla2tvUmVhY3ROZXhKc1xcXFxwZWtrb190ZW1wbGF0ZVxcXFxhc3NldHNcXFxcaW1hZ2VzXFxcXGFib3V0LmpwZ1wiKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=H%3A%5Cwebsoham%5Cpekkoreact-10%5Cpekkoreact-10%5CPekkoReactNexJs%5Cpekko_template%5Cassets%5Cimages%5Cabout.jpg&server=false!\n"));
+
+/***/ }),
+
+/***/ "(app-pages-browser)/./assets/images/about.jpg":
+/*!*********************************!*\
+  !*** ./assets/images/about.jpg ***!
+  \*********************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\"src\":\"/_next/static/media/about.6d904730.jpg\",\"height\":700,\"width\":1440,\"blurDataURL\":\"/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fabout.6d904730.jpg&w=8&q=70\",\"blurWidth\":8,\"blurHeight\":4});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL2Fzc2V0cy9pbWFnZXMvYWJvdXQuanBnIiwibWFwcGluZ3MiOiI7QUFBQSwrREFBZSxDQUFDLCtMQUErTCIsInNvdXJjZXMiOlsid2VicGFjazovL19OX0UvLi9hc3NldHMvaW1hZ2VzL2Fib3V0LmpwZz9lYTFjIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IHtcInNyY1wiOlwiL19uZXh0L3N0YXRpYy9tZWRpYS9hYm91dC42ZDkwNDczMC5qcGdcIixcImhlaWdodFwiOjcwMCxcIndpZHRoXCI6MTQ0MCxcImJsdXJEYXRhVVJMXCI6XCIvX25leHQvaW1hZ2U/dXJsPSUyRl9uZXh0JTJGc3RhdGljJTJGbWVkaWElMkZhYm91dC42ZDkwNDczMC5qcGcmdz04JnE9NzBcIixcImJsdXJXaWR0aFwiOjgsXCJibHVySGVpZ2h0XCI6NH07Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(app-pages-browser)/./assets/images/about.jpg\n"));
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ __webpack_require__.O(0, ["main-app"], function() { return __webpack_exec__("(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=H%3A%5Cwebsoham%5Cpekkoreact-10%5Cpekkoreact-10%5CPekkoReactNexJs%5Cpekko_template%5Cassets%5Cimages%5Cabout.jpg&server=false!"); });
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ _N_E = __webpack_exports__;
+/******/ }
+]);
--- app/page.tsx
+++ app/page.tsx
@@ -23,193 +23,236 @@ import closeIcon from "@/assets/images/close.png";
 // ------------
 
 function Home() {
-  const [openPortfolio, setOpenPortfolio] = useState<number>(0);
+	const [openPortfolio, setOpenPortfolio] = useState<number>(0);
 
-  /**
-   * Open a popup of the item with the given number passed to the function
-   *
-   * @param num Pop up item number to be open
-   */
-  const handleOpenPopup = (num: number) => {
-    setOpenPortfolio(num);
-  };
+	/**
+	 * Open a popup of the item with the given number passed to the function
+	 *
+	 * @param num Pop up item number to be open
+	 */
+	const handleOpenPopup = (num: number) => {
+		setOpenPortfolio(num);
+	};
 
-  /**
-   * Closed the opened items by reseting the {openPortfolio} variable to 0
-   */
-  const handleClosePopup = () => {
-    setOpenPortfolio(0);
-  };
+	/**
+	 * Closed the opened items by reseting the {openPortfolio} variable to 0
+	 */
+	const handleClosePopup = () => {
+		setOpenPortfolio(0);
+	};
 
-  return (
-    <>
-      <main className="page-background">
-        <div id="content" className="site-content">
-          <div className="content-holder center-relative content-1170">
-            <div id="portfolio-wrapper">
-              <div className="our-grid">
-                <motion.div
-                  className="our-grid-item d-1x2 animate"
-                  initial={{ opacity: 0, transform: `translateY(50px)` }}
-                  whileInView={{ opacity: 1, transform: `translateY(0px)` }}
-                  viewport={{ once: true }}
-                >
-                  <Link className="item-link" href="/single-portfolio">
-                    <img src={img01.src} alt="" />
-                    <div className="portfolio-text-holder">
-                      <p className="portfolio-title">PHOTOGRAPHY</p>
-                    </div>
-                  </Link>
-                </motion.div>
+	return (
+		<>
+			<main className='page-background'>
+				<div
+					id='content'
+					className='site-content'>
+					<div className='content-holder center-relative content-1170'>
+						<div id='portfolio-wrapper'>
+							<div className='our-grid'>
+								<motion.div
+									className='our-grid-item d-1x2 animate'
+									initial={{ opacity: 0, transform: `translateY(50px)` }}
+									whileInView={{ opacity: 1, transform: `translateY(0px)` }}
+									viewport={{ once: true }}>
+									<Link
+										className='item-link'
+										href='/single-portfolio'>
+										<img
+											src={img01.src}
+											alt=''
+										/>
+										<div className='portfolio-text-holder'>
+											<p className='portfolio-title'>PHOTOGRAPHY</p>
+										</div>
+									</Link>
+								</motion.div>
 
-                <motion.div
-                  className="our-grid-item d-1x1 animate"
-                  initial={{ opacity: 0, transform: `translateY(50px)` }}
-                  whileInView={{ opacity: 1, transform: `translateY(0px)` }}
-                  viewport={{ once: true }}
-                >
-                  <a className="item-link" onClick={() => handleOpenPopup(1)}>
-                    <img src={img02.src} alt="" />
-                    <div className="portfolio-text-holder">
-                      <p className="portfolio-title">MOCKUP</p>
-                    </div>
-                  </a>
-                </motion.div>
+								<motion.div
+									className='our-grid-item d-1x1 animate'
+									initial={{ opacity: 0, transform: `translateY(50px)` }}
+									whileInView={{ opacity: 1, transform: `translateY(0px)` }}
+									viewport={{ once: true }}>
+									<a
+										className='item-link'
+										onClick={() => handleOpenPopup(1)}>
+										<img
+											src={img02.src}
+											alt=''
+										/>
+										<div className='portfolio-text-holder'>
+											<p className='portfolio-title'>MOCKUP</p>
+										</div>
+									</a>
+								</motion.div>
 
-                <motion.div
-                  className="our-grid-item d-1x1 animate"
-                  initial={{ opacity: 0, transform: `translateY(50px)` }}
-                  whileInView={{ opacity: 1, transform: `translateY(0px)` }}
-                  viewport={{ once: true }}
-                >
-                  <a className="item-link" onClick={() => handleOpenPopup(2)}>
-                    <img src={img03.src} alt="" />
-                    <div className="portfolio-text-holder">
-                      <p className="portfolio-title">SNEAKERS</p>
-                    </div>
-                  </a>
-                </motion.div>
+								<motion.div
+									className='our-grid-item d-1x1 animate'
+									initial={{ opacity: 0, transform: `translateY(50px)` }}
+									whileInView={{ opacity: 1, transform: `translateY(0px)` }}
+									viewport={{ once: true }}>
+									<a
+										className='item-link'
+										onClick={() => handleOpenPopup(2)}>
+										<img
+											src={img03.src}
+											alt=''
+										/>
+										<div className='portfolio-text-holder'>
+											<p className='portfolio-title'>SNEAKERS</p>
+										</div>
+									</a>
+								</motion.div>
 
-                <motion.div
-                  className="our-grid-item d-2x1 animate"
-                  initial={{ opacity: 0, transform: `translateY(50px)` }}
-                  whileInView={{ opacity: 1, transform: `translateY(0px)` }}
-                  viewport={{ once: true }}
-                >
-                  <a className="item-link" onClick={() => handleOpenPopup(3)}>
-                    <img src={img04.src} alt="" />
-                    <div className="portfolio-text-holder">
-                      <p className="portfolio-title">ARCHITECTURE</p>
-                    </div>
-                  </a>
-                </motion.div>
+								<motion.div
+									className='our-grid-item d-2x1 animate'
+									initial={{ opacity: 0, transform: `translateY(50px)` }}
+									whileInView={{ opacity: 1, transform: `translateY(0px)` }}
+									viewport={{ once: true }}>
+									<a
+										className='item-link'
+										onClick={() => handleOpenPopup(3)}>
+										<img
+											src={img04.src}
+											alt=''
+										/>
+										<div className='portfolio-text-holder'>
+											<p className='portfolio-title'>ARCHITECTURE</p>
+										</div>
+									</a>
+								</motion.div>
 
-                <motion.div
-                  className="our-grid-item d-1x1 animate"
-                  initial={{ opacity: 0, transform: `translateY(50px)` }}
-                  whileInView={{ opacity: 1, transform: `translateY(0px)` }}
-                  viewport={{ once: true }}
-                >
-                  <Link className="item-link" href="/single-portfolio">
-                    <img src={img05.src} alt="" />
-                    <div className="portfolio-text-holder">
-                      <p className="portfolio-title">ART</p>
-                    </div>
-                  </Link>
-                </motion.div>
+								<motion.div
+									className='our-grid-item d-1x1 animate'
+									initial={{ opacity: 0, transform: `translateY(50px)` }}
+									whileInView={{ opacity: 1, transform: `translateY(0px)` }}
+									viewport={{ once: true }}>
+									<Link
+										className='item-link'
+										href='/single-portfolio'>
+										<img
+											src={img05.src}
+											alt=''
+										/>
+										<div className='portfolio-text-holder'>
+											<p className='portfolio-title'>ART</p>
+										</div>
+									</Link>
+								</motion.div>
 
-                <motion.div
-                  className="our-grid-item d-1x2 animate"
-                  initial={{ opacity: 0, transform: `translateY(50px)` }}
-                  whileInView={{ opacity: 1, transform: `translateY(0px)` }}
-                  viewport={{ once: true }}
-                >
-                  <a className="item-link" onClick={() => handleOpenPopup(4)}>
-                    <img src={img06.src} alt="" />
-                    <div className="portfolio-text-holder">
-                      <p className="portfolio-title">OFFICE</p>
-                    </div>
-                  </a>
-                </motion.div>
+								<motion.div
+									className='our-grid-item d-1x2 animate'
+									initial={{ opacity: 0, transform: `translateY(50px)` }}
+									whileInView={{ opacity: 1, transform: `translateY(0px)` }}
+									viewport={{ once: true }}>
+									<a
+										className='item-link'
+										onClick={() => handleOpenPopup(4)}>
+										<img
+											src={img06.src}
+											alt=''
+										/>
+										<div className='portfolio-text-holder'>
+											<p className='portfolio-title'>OFFICE</p>
+										</div>
+									</a>
+								</motion.div>
 
-                <motion.div
-                  className="our-grid-item d-1x2 animate"
-                  initial={{ opacity: 0, transform: `translateY(50px)` }}
-                  whileInView={{ opacity: 1, transform: `translateY(0px)` }}
-                  viewport={{ once: true }}
-                >
-                  <Link className="item-link" href="/single-portfolio">
-                    <img src={img07.src} alt="" />
-                    <div className="portfolio-text-holder">
-                      <p className="portfolio-title">MODEL</p>
-                    </div>
-                  </Link>
-                </motion.div>
+								<motion.div
+									className='our-grid-item d-1x2 animate'
+									initial={{ opacity: 0, transform: `translateY(50px)` }}
+									whileInView={{ opacity: 1, transform: `translateY(0px)` }}
+									viewport={{ once: true }}>
+									<Link
+										className='item-link'
+										href='/single-portfolio'>
+										<img
+											src={img07.src}
+											alt=''
+										/>
+										<div className='portfolio-text-holder'>
+											<p className='portfolio-title'>MODEL</p>
+										</div>
+									</Link>
+								</motion.div>
 
-                <motion.div
-                  className="our-grid-item d-1x1 animate"
-                  initial={{ opacity: 0, transform: `translateY(50px)` }}
-                  whileInView={{ opacity: 1, transform: `translateY(0px)` }}
-                  viewport={{ once: true }}
-                >
-                  <Link className="item-link" href="/single-portfolio">
-                    <img src={img08.src} alt="" />
-                    <div className="portfolio-text-holder">
-                      <p className="portfolio-title">MOCKUP</p>
-                    </div>
-                  </Link>
-                </motion.div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
-      {/* Popups */}
-      <Popup
-        open={openPortfolio !== 0}
-        closeOnDocumentClick
-        onClose={handleClosePopup}
-        modal
-      >
-        <div className="my-popup">
-          <div
-            className="close-popup-btn"
-            role="button"
-            onClick={handleClosePopup}
-          >
-            <img src={closeIcon.src} alt="close icon" />
-          </div>
-          {openPortfolio === 1 ? (
-            <div className="popup-image-box">
-              <img src={img02B.src} alt="portfolio image" />
-            </div>
-          ) : openPortfolio === 2 ? (
-            <p className="video-framer poped-up-item" onClick={close}>
-              <iframe
-                src="https://player.vimeo.com/video/199192931"
-                width="100%"
-                allow="autoplay; fullscreen"
-              ></iframe>
-            </p>
-          ) : openPortfolio === 3 ? (
-            <div className="popup-image-box">
-              <img src={img04.src} alt="portfolio image" />
-            </div>
-          ) : openPortfolio === 4 ? (
-            <p className="video-framer poped-up-item" onClick={close}>
-              <iframe
-                src="https://player.vimeo.com/video/199192931"
-                width="100%"
-                allow="autoplay; fullscreen"
-              ></iframe>
-            </p>
-          ) : (
-            <></>
-          )}
-        </div>
-      </Popup>
-    </>
-  );
+								<motion.div
+									className='our-grid-item d-1x1 animate'
+									initial={{ opacity: 0, transform: `translateY(50px)` }}
+									whileInView={{ opacity: 1, transform: `translateY(0px)` }}
+									viewport={{ once: true }}>
+									<Link
+										className='item-link'
+										href='/single-portfolio'>
+										<img
+											src={img08.src}
+											alt=''
+										/>
+										<div className='portfolio-text-holder'>
+											<p className='portfolio-title'>MOCKUP</p>
+										</div>
+									</Link>
+								</motion.div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
+			{/* Popups */}
+			<Popup
+				open={openPortfolio !== 0}
+				closeOnDocumentClick
+				onClose={handleClosePopup}
+				modal>
+				<div className='my-popup'>
+					<div
+						className='close-popup-btn'
+						role='button'
+						onClick={handleClosePopup}>
+						<img
+							src={closeIcon.src}
+							alt='close icon'
+						/>
+					</div>
+					{openPortfolio === 1 ? (
+						<div className='popup-image-box'>
+							<img
+								src={img02B.src}
+								alt='portfolio image'
+							/>
+						</div>
+					) : openPortfolio === 2 ? (
+						<p
+							className='video-framer poped-up-item'
+							onClick={close}>
+							<iframe
+								src='https://player.vimeo.com/video/199192931'
+								width='100%'
+								allow='autoplay; fullscreen'></iframe>
+						</p>
+					) : openPortfolio === 3 ? (
+						<div className='popup-image-box'>
+							<img
+								src={img04.src}
+								alt='portfolio image'
+							/>
+						</div>
+					) : openPortfolio === 4 ? (
+						<p
+							className='video-framer poped-up-item'
+							onClick={close}>
+							<iframe
+								src='https://player.vimeo.com/video/199192931'
+								width='100%'
+								allow='autoplay; fullscreen'></iframe>
+						</p>
+					) : (
+						<></>
+					)}
+				</div>
+			</Popup>
+		</>
+	);
 }
 
 export default Home;
